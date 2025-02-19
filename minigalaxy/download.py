3bee29a4280562554a4a3e0490f21259a551b807
GB609 <39741460+GB609@users.noreply.github.com>
2025-02-19 10:39:38 +0000

    Enhance Download API - step 1
    1. Change parameters passed to callbacks
    2. Add an additional callback_context property to avoid the need for lambdas in some situations, as all required data to decide which action to take can be assigned to the download. This allows class members or static functions to be used as callbacks and 'arguments' or variables can be taken out of the download.
    3. Add a few more explanations in the usage doc
    4. rename member variables

--- minigalaxy/download.py
+++ minigalaxy/download.py
@@ -23,16 +23,33 @@ class Download:
     >>>   image_url = "https://www.gog.com/bundles/gogwebsitestaticpages/images/icon_section1-header.png"
     >>>   thumbnail = os.path.join(".", "{}.jpg".format("test-icon"))
     >>>   download = Download(image_url, thumbnail, DownloadType.THUMBNAIL, finish_func=lambda x: print("Done downloading {}!".format(x))) # noqa: E501
+    >>>   download_manager.download(download)
     >>> your_function() # doctest: +SKIP
+
+    API:
+    Three different callback hooks can be defined when constructing an instance. 
+    These expect the passed in function to be able to handle the following signature:
+    - finish_func(Download): Called when the download completes successfully
+    - progress_func(Download, percentage: int): 
+      If the file size is known, this will be called whenever a new chunk is downloaded. If not, only at the beginning (0) and end (100)
+    - cancel_func(Download, reason: str): When a download is aborted, either manually or because of an error during download
+
+    Additional optional parameters:
+    - download_type: Effectively determines queue to use and thus might impact priority
+    - game: Game object this download is associated with
+    - callback_context: 
+      Arbitrary data that can be used as additional input in callbacks.
+      Useful to avoid the need for nested lambdas
     """
-    def __init__(self, url, save_location, download_type=None, finish_func=None,
-                 progress_func=None, cancel_func=None, number=1,
-                 out_of_amount=1, game=None):
+    def __init__(self, url, save_location, download_type=None, 
+                 finish_func=None, progress_func=None, cancel_func=None, 
+                 number=1, out_of_amount=1, game=None, callback_context=None):
         self.url = url
         self.save_location = save_location
-        self.__finish_func = finish_func
-        self.__progress_func = progress_func
-        self.__cancel_func = cancel_func
+        self.callback_complete = finish_func
+        self.callback_progress = progress_func
+        self.callback_cancel = cancel_func
+        self.callback_context = callback_context
         self.number = number
         self.out_of_amount = out_of_amount
         self.game = game
@@ -41,26 +58,29 @@ class Download:
 
     def set_progress(self, percentage: int) -> None:
         "Set the download progress of the Download"
-        if self.__progress_func:
+        if self.callback_progress:
             if self.out_of_amount > 1:
                 # Change the percentage based on which number we are
                 progress_start = 100 / self.out_of_amount * (self.number - 1)
                 percentage = progress_start + percentage / self.out_of_amount
                 percentage = int(percentage)
-            self.__progress_func(percentage)
+            self.callback_progress(self, percentage)
 
     def finish(self):
         """
         finish is called when the download has completed
         If a finish_func was specified when the Download was created, call the function
         """
-        if self.__finish_func:
+        if self.callback_complete:
             try:
-                self.__finish_func(self.save_location)
-            except (FileNotFoundError, BadZipFile):
-                self.cancel()
+                self.callback_complete(self)
+            except (FileNotFoundError, BadZipFile) as error:
+                self.cancel(str(error))
 
-    def cancel(self):
+    def cancel(self, reason=None):
         "Cancel the download, calling a cancel_func if one was specified"
-        if self.__cancel_func:
-            self.__cancel_func()
+        if self.callback_cancel:
+            self.callback_cancel(self, reason)
+
+    def _enqueue(self, download_manager):
+        download_manager.put_in_proper_queue(self)
